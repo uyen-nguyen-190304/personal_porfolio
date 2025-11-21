df31f8ec3c67dac769f82d63e6eb82eab05ca525
Uyen Nguyen <nguyen_u1@denison.edu>
2025-11-21 08:06:06 -0500

    add: insert education section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import Education from "@/components/Education";
 import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
@@ -13,6 +14,7 @@ const Index = () => {
       <main>
         <Hero />
         <About />
+        <Education />
         <Experience />
         <Projects />
         <Contact />
